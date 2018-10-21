429c328dcde5eb3d34c8eeb4904c353a3f1d84d0
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-10-21 23:07:19 +0800

    response not found when route not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -45,6 +46,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof NotFoundHttpException){
+            return response()->json([
+                'success'   => false,
+                'messages'  => 'Not Found!'
+            ], 404);
+        }
         return parent::render($request, $exception);
     }
 }
