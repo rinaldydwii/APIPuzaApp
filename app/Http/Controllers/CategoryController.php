6bb8456ca56572bf5095e24524b45e132520d814
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-10-25 23:54:40 +0800

    update category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Category;
+use App\Log;
 
 class CategoryController extends Controller
 {
@@ -21,7 +23,11 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return response()->json(Category::all());
+        return response()->json([
+            'success'   => true,
+            'messages'  => 'List of All Categories',
+            'data'      => Category::all()
+        ], 200);
     }
     /**
      * Store a newly created resource in storage.
@@ -33,11 +39,29 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|max:15|unique:categories',
+            'name' => 'required|max:50|unique:categories',
         ]);
-        $category = Category::create($request->all());
 
-        return response()->json($category, 201);
+        $category = Category::create($request->all());
+        
+        if ($category) {
+            $log = Log::create([
+                'log_sub_type_id'   => env('LOG_CATEGORY_CREATE'),
+                'user_id'           => Auth::user() -> id,
+                'information'       => 'Create new category: ' . $category -> name
+            ]);
+            return response()->json([
+                'success'   => true,
+                'messages'  => 'Create new category: '.$category -> name,
+                'data'      => $category
+            ], 200);
+        } else {
+            return response()->json([
+                'success'   => false,
+                'messages'  => 'Create Category Fail!',
+            ], 400);
+        }
+        
     }
     /**
      * Display the specified resource.
@@ -48,7 +72,11 @@ class CategoryController extends Controller
      */
     public function show($id)
     {
-        return response()->json(Category::find($id));
+        return response()->json([
+            'success'   => true,
+            'messages'  => 'Detail of Category',
+            'data'      => Category::find($id)
+        ], 200);
     }
     /**
      * Update the specified resource in storage.
@@ -60,10 +88,38 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'name' => 'required|max:50|unique:categories,name,'.$id,
+        ]);
+
         $category = Category::findOrFail($id);
+        $logInfo = 'Update Category';
+        $changed = false;
+        
+        if ($request -> name != $category -> name ) {
+            $logInfo .= ' \n Name: ' . $category -> name . ' into ' . $request -> name;
+            $changed = true;
+        }
+
+        if (!$changed) {
+            return response()->json([
+                'success'   => false,
+                'messages'  => 'No Category Changes!',
+            ], 400);
+        }
+
         $category -> update($request->all());
 
-        return response()->json($category, 200);
+        $log = Log::create([
+            'log_sub_type_id'   => env('LOG_CATEGORY_UPDATE'),
+            'user_id'           => Auth::user() -> id,
+            'information'       => $logInfo
+        ]);
+        return response()->json([
+            'success'   => true,
+            'messages'  => 'Update Category Successfully!',
+            'data'      => $category
+        ], 200);
     }
     /**
      * Remove the specified resource from storage.
@@ -74,10 +130,18 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        Category::findOrFail($id) -> delete();
-        return response()->json([
-            'success'   => true,
-            'messages'  => 'Delete Successfully!',
-        ], 200);
+        $category = Category::findOrFail($id);
+
+        if ($category -> delete()) {
+            $log = Log::create([
+                'log_sub_type_id'   => env('LOG_CATEGORY_DELETE'),
+                'user_id'           => Auth::user() -> id,
+                'information'       => 'Delete category: '.$category -> name
+            ]);
+            return response()->json([
+                'success'   => true,
+                'messages'  => 'Delete Category Successfully!',
+            ], 200);
+        }
     }
 }
